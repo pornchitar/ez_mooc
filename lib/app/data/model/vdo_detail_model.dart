3cf28f04a0b84d942c902fa5532dc411e0213a23
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-12-09 12:58:18 +0700

    modify vdo model

--- lib/app/data/model/vdo_detail_model.dart
+++ lib/app/data/model/vdo_detail_model.dart
@@ -1,3 +1,5 @@
+import 'package:ez_mooc/app/data/model/category_model.dart';
+
 class VdoDetail {
   int videoId;
   int subjectId;
@@ -8,31 +10,33 @@ class VdoDetail {
   String videoCode;
   String createdAt;
   String updatedAt;
+  List<Category> category;
 
-  VdoDetail({
-    required this.videoId,
-    required this.subjectId,
-    required this.videoTitle,
-    required this.videoURL,
-    required this.thumbnail,
-    required this.channelName,
-    required this.videoCode,
-    required this.createdAt,
-    required this.updatedAt,
-  });
+  VdoDetail(
+      {required this.videoId,
+      required this.subjectId,
+      required this.videoTitle,
+      required this.videoURL,
+      required this.thumbnail,
+      required this.channelName,
+      required this.videoCode,
+      required this.createdAt,
+      required this.updatedAt,
+      required this.category});
 
   factory VdoDetail.fromJson(Map<String, dynamic> json) {
     return VdoDetail(
-      videoId: json['VideoID'],
-      subjectId: json['SubjectID'],
-      videoTitle: json['VideoTitle'],
-      videoURL: json['VideoURL'],
-      thumbnail: json['Thumbnail'],
-      channelName: json['ChannelName'],
-      videoCode: json['VideoCode'],
-      createdAt: json['created_at'],
-      updatedAt: json['updated_at'],
-    );
+        videoId: json['VideoID'],
+        subjectId: json['SubjectID'],
+        videoTitle: json['VideoTitle'],
+        videoURL: json['VideoURL'],
+        thumbnail: json['Thumbnail'],
+        channelName: json['ChannelName'],
+        videoCode: json['VideoCode'],
+        createdAt: json['created_at'],
+        updatedAt: json['updated_at'],
+        category: List<Category>.from(
+            json['categories'].map((x) => Category.fromJson(x))));
   }
   //to json
   Map<String, dynamic> toJson() => {
